fb6abee6c5f17f45f33d3cce9621fcda670599ab
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-28 10:47:28 -0300

    added get and create user

---
+++ lib/wabanex/users/get.ex
@@ -0,0 +1,18 @@
+defmodule Wabanex.Users.Get do
+  alias Ecto.UUID
+  alias Wabanex.{User, Repo}
+
+  def call(id) do
+  end
+
+  defp handle_response(:error) do
+    {:error, "Invalid UUID"}
+  end
+
+  defp handle_response({:ok, uuid}) do
+    case Repo.get(User, uuid) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+end
