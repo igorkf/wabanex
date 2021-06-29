391da85a6cbda4f8f998a390e0ee3760d7608db5
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-29 12:38:04 -0300

    changed users to has many and added preload

--- lib/wabanex/training.ex
+++ lib/wabanex/training.ex
@@ -22,7 +22,6 @@ defmodule Wabanex.Training do
     %__MODULE__{}
     |> cast(params, @fields)
     |> validate_required(@fields)
-    |> unique_constraint([:email])
     |> cast_assoc(:exercises)
   end
 end
